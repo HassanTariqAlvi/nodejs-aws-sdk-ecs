55c2de383598ddd9b94be067702c1010bd094a5b
Muhammad Hassan Tariq <97219404+HassanTariqAlvi@users.noreply.github.com>
2023-11-15 13:27:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 
 
 AWS.config.update({
-  region: process.env.AWS_REGION, 
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID, 
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
@@ -32,26 +32,51 @@ app.get('/task-metadata', async (req, res) => {
 
 app.post('/stop-task', async (req, res) => {
   try {
-      const taskArn = req.body.taskArn;
-      const cluster = req.body.cluster; // Make sure to pass the cluster name in the request
+      // Fetch ECS task metadata
+      const metadataResponse = await axios.get('http://169.254.170.2/v2/metadata');
+      const taskMetadata = metadataResponse.data;
 
-      await ecs.stopTask({ cluster, task: taskArn }).promise();
+      // Extract the task ARN from metadata
+      const taskArn = taskMetadata.TaskARN;
+
+      // Extract the cluster name from the Cluster ARN
+      const clusterArn = taskMetadata.Cluster;
+      const clusterName = clusterArn.split(':')[5].split('/')[1];
+
+      // Stop the task using ECS SDK
+      await ecs.stopTask({ cluster: clusterName, task: taskArn }).promise();
 
       return res.json({ message: 'Task stopped successfully' });
   } catch (error) {
+      console.error("Error stopping task: ", error);
       return res.status(500).json({ error: error.message });
   }
 });
+
+
+app.get('/end-myself', async (req, res) => {
+  try {
+    // Making a POST request to the /stop-task endpoint
+    const stopTaskResponse = await axios.post('http://localhost:3000/stop-task');
+
+    // Sending the response back to the client
+    res.json(stopTaskResponse.data);
+  } catch (error) {
+    console.error("Error in ending self: ", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
 //ADED for 2nd task
 
 
-function updateECSService(desiredCount) {
-  const params = {
-    cluster: 'DevClusterDI-Bot', 
-    service: 'nginx-service', 
+  function updateECSService(desiredCount) {
+   const params = {
+    cluster: process.env.ECS_CLUSTER,
+    service: process.env.ECS_SERVICE,
     desiredCount: desiredCount
   };
-
   ecs.updateService(params, function(err, data) {
     if (err) {
       console.error("Error", err);
@@ -63,13 +88,13 @@ function updateECSService(desiredCount) {
 
 
 app.get('/start', (req, res) => {
-  updateECSService(3); 
+  updateECSService(3);
   res.send('Task count increased to 3');
 });
 
 
 app.get('/end', (req, res) => {
-  updateECSService(1); // 
+  updateECSService(1); //
   res.send('Task count decreased to 1');
 });
 
